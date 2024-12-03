9552b9eb5f1d9909681fd36c382977adc84a76c5
jagadw <jagadhitawicaksana@gmail.com>
2024-12-03 21:55:33 +0800

    Create simple views

---
+++ resources/views/books.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>List Of Books</title>
+</head>
+<body>
+    <form action="">
+        <p>List shown : <input type="number"></p>
+        <p>Search     : <input type="text"></p>
+    </form>
+    <table border="1" align="center">
+        <tr>
+            <th>No</th>
+            <th>Book Name</th>
+            <th>Category Name</th>
+            <th>Author Name</th>
+            <th>Average Rating</th>
+            <th>Voter</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ resources/views/rating.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rating</title>
+</head>
+<body>
+    <h1>Insert Rating</h1>
+    <p>Book Author : <select name="" id=""></select></p>
+    <p>Book name : <select name="" id=""></select></p>
+    <p>Rating : <input type="number"></p>
+</body>
+</html>
